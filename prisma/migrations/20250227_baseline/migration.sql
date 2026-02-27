3eb939f827e21a2a0b51166bbb023a0ed5ee5d0e
Noah Klein <noah2klein16@gmail.com>
2026-02-27 16:36:17 -0500

    Initial migration

---
+++ prisma/migrations/20250227_baseline/migration.sql
@@ -0,0 +1,224 @@
+﻿-- CreateSchema
+CREATE SCHEMA IF NOT EXISTS "neon_auth";
+
+-- CreateSchema
+CREATE SCHEMA IF NOT EXISTS "public";
+
+-- CreateTable
+CREATE TABLE "scenario" (
+    "id" VARCHAR(255) NOT NULL,
+    "user_id" INTEGER NOT NULL,
+    "title" VARCHAR(255) NOT NULL,
+    "scenario_version" INTEGER NOT NULL,
+    "content" JSONB NOT NULL,
+    "created_at" TIMESTAMP(6) DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(6) DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "scenario_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "submission" (
+    "id" SERIAL NOT NULL,
+    "user_id" INTEGER NOT NULL,
+    "scenario_id" VARCHAR(255) NOT NULL,
+    "node_id" VARCHAR(255) NOT NULL,
+    "question_prompt" TEXT NOT NULL,
+    "user_response_text" TEXT NOT NULL,
+    "bucket_id" VARCHAR(255),
+    "feedback" TEXT,
+    "created_at" TIMESTAMP(6) DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "submission_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "neon_auth"."account" (
+    "id" UUID NOT NULL DEFAULT gen_random_uuid(),
+    "accountId" TEXT NOT NULL,
+    "providerId" TEXT NOT NULL,
+    "userId" UUID NOT NULL,
+    "accessToken" TEXT,
+    "refreshToken" TEXT,
+    "idToken" TEXT,
+    "accessTokenExpiresAt" TIMESTAMPTZ(6),
+    "refreshTokenExpiresAt" TIMESTAMPTZ(6),
+    "scope" TEXT,
+    "password" TEXT,
+    "createdAt" TIMESTAMPTZ(6) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMPTZ(6) NOT NULL,
+
+    CONSTRAINT "account_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "neon_auth"."invitation" (
+    "id" UUID NOT NULL DEFAULT gen_random_uuid(),
+    "organizationId" UUID NOT NULL,
+    "email" TEXT NOT NULL,
+    "role" TEXT,
+    "status" TEXT NOT NULL,
+    "expiresAt" TIMESTAMPTZ(6) NOT NULL,
+    "createdAt" TIMESTAMPTZ(6) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "inviterId" UUID NOT NULL,
+
+    CONSTRAINT "invitation_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "neon_auth"."jwks" (
+    "id" UUID NOT NULL DEFAULT gen_random_uuid(),
+    "publicKey" TEXT NOT NULL,
+    "privateKey" TEXT NOT NULL,
+    "createdAt" TIMESTAMPTZ(6) NOT NULL,
+    "expiresAt" TIMESTAMPTZ(6),
+
+    CONSTRAINT "jwks_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "neon_auth"."member" (
+    "id" UUID NOT NULL DEFAULT gen_random_uuid(),
+    "organizationId" UUID NOT NULL,
+    "userId" UUID NOT NULL,
+    "role" TEXT NOT NULL,
+    "createdAt" TIMESTAMPTZ(6) NOT NULL,
+
+    CONSTRAINT "member_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "neon_auth"."organization" (
+    "id" UUID NOT NULL DEFAULT gen_random_uuid(),
+    "name" TEXT NOT NULL,
+    "slug" TEXT NOT NULL,
+    "logo" TEXT,
+    "createdAt" TIMESTAMPTZ(6) NOT NULL,
+    "metadata" TEXT,
+
+    CONSTRAINT "organization_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "neon_auth"."project_config" (
+    "id" UUID NOT NULL DEFAULT gen_random_uuid(),
+    "name" TEXT NOT NULL,
+    "endpoint_id" TEXT NOT NULL,
+    "created_at" TIMESTAMPTZ(6) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMPTZ(6) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "trusted_origins" JSONB NOT NULL,
+    "social_providers" JSONB NOT NULL,
+    "email_provider" JSONB,
+    "email_and_password" JSONB,
+    "allow_localhost" BOOLEAN NOT NULL,
+    "plugin_configs" JSONB,
+    "webhook_config" JSONB,
+
+    CONSTRAINT "project_config_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "neon_auth"."session" (
+    "id" UUID NOT NULL DEFAULT gen_random_uuid(),
+    "expiresAt" TIMESTAMPTZ(6) NOT NULL,
+    "token" TEXT NOT NULL,
+    "createdAt" TIMESTAMPTZ(6) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMPTZ(6) NOT NULL,
+    "ipAddress" TEXT,
+    "userAgent" TEXT,
+    "userId" UUID NOT NULL,
+    "impersonatedBy" TEXT,
+    "activeOrganizationId" TEXT,
+
+    CONSTRAINT "session_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "neon_auth"."user" (
+    "id" UUID NOT NULL DEFAULT gen_random_uuid(),
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "emailVerified" BOOLEAN NOT NULL,
+    "image" TEXT,
+    "createdAt" TIMESTAMPTZ(6) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMPTZ(6) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "role" TEXT,
+    "banned" BOOLEAN,
+    "banReason" TEXT,
+    "banExpires" TIMESTAMPTZ(6),
+
+    CONSTRAINT "user_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "neon_auth"."verification" (
+    "id" UUID NOT NULL DEFAULT gen_random_uuid(),
+    "identifier" TEXT NOT NULL,
+    "value" TEXT NOT NULL,
+    "expiresAt" TIMESTAMPTZ(6) NOT NULL,
+    "createdAt" TIMESTAMPTZ(6) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMPTZ(6) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "verification_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE INDEX "idx_scenarios_user_id" ON "scenario"("user_id");
+
+-- CreateIndex
+CREATE INDEX "idx_submissions_scenario_id" ON "submission"("scenario_id");
+
+-- CreateIndex
+CREATE INDEX "idx_submissions_user_id" ON "submission"("user_id");
+
+-- CreateIndex
+CREATE INDEX "account_userId_idx" ON "neon_auth"."account"("userId");
+
+-- CreateIndex
+CREATE INDEX "invitation_email_idx" ON "neon_auth"."invitation"("email");
+
+-- CreateIndex
+CREATE INDEX "invitation_organizationId_idx" ON "neon_auth"."invitation"("organizationId");
+
+-- CreateIndex
+CREATE INDEX "member_organizationId_idx" ON "neon_auth"."member"("organizationId");
+
+-- CreateIndex
+CREATE INDEX "member_userId_idx" ON "neon_auth"."member"("userId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "organization_slug_key" ON "neon_auth"."organization"("slug");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "project_config_endpoint_id_key" ON "neon_auth"."project_config"("endpoint_id");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "session_token_key" ON "neon_auth"."session"("token");
+
+-- CreateIndex
+CREATE INDEX "session_userId_idx" ON "neon_auth"."session"("userId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "user_email_key" ON "neon_auth"."user"("email");
+
+-- CreateIndex
+CREATE INDEX "verification_identifier_idx" ON "neon_auth"."verification"("identifier");
+
+-- AddForeignKey
+ALTER TABLE "neon_auth"."account" ADD CONSTRAINT "account_userId_fkey" FOREIGN KEY ("userId") REFERENCES "neon_auth"."user"("id") ON DELETE CASCADE ON UPDATE NO ACTION;
+
+-- AddForeignKey
+ALTER TABLE "neon_auth"."invitation" ADD CONSTRAINT "invitation_inviterId_fkey" FOREIGN KEY ("inviterId") REFERENCES "neon_auth"."user"("id") ON DELETE CASCADE ON UPDATE NO ACTION;
+
+-- AddForeignKey
+ALTER TABLE "neon_auth"."invitation" ADD CONSTRAINT "invitation_organizationId_fkey" FOREIGN KEY ("organizationId") REFERENCES "neon_auth"."organization"("id") ON DELETE CASCADE ON UPDATE NO ACTION;
+
+-- AddForeignKey
+ALTER TABLE "neon_auth"."member" ADD CONSTRAINT "member_organizationId_fkey" FOREIGN KEY ("organizationId") REFERENCES "neon_auth"."organization"("id") ON DELETE CASCADE ON UPDATE NO ACTION;
+
+-- AddForeignKey
+ALTER TABLE "neon_auth"."member" ADD CONSTRAINT "member_userId_fkey" FOREIGN KEY ("userId") REFERENCES "neon_auth"."user"("id") ON DELETE CASCADE ON UPDATE NO ACTION;
+
+-- AddForeignKey
+ALTER TABLE "neon_auth"."session" ADD CONSTRAINT "session_userId_fkey" FOREIGN KEY ("userId") REFERENCES "neon_auth"."user"("id") ON DELETE CASCADE ON UPDATE NO ACTION;
+
